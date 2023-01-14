556189e357e82926e6ef07ea62d935d658395c0c
urswilke <urs.wilke@gmail.com>
2023-01-14 15:31:10 +0100

    replace invalid sample names

--- R/tidal_to_strudel.R
+++ R/tidal_to_strudel.R
@@ -1,27 +1,67 @@
 library(tidyverse)
 
 filename <- "tidal-drum-patterns/Sound/Tidal/Drum/Trap1a.hs"
-extract_patterns <- function(filename) {
+extract_instr <- function(filename) {
   pattern_strings <- read_lines(filename) |>
     str_subset('".*"')
   instruments <- pattern_strings |>
     str_remove(" = .*")
-  pattern_strings |>
-    str_extract('(?<=").*(?=")') |>
-    str_replace_all("t", instruments) %>%
-    paste0('"', ., '",')
+  patterns <- pattern_strings |>
+    str_extract('(?<=").*(?=")')
+  set_names(patterns, instruments)
 }
+
 
 # extract_patterns(filename)
 
-l <- fs::dir_ls("tidal-drum-patterns/Sound/Tidal/Drum/") |>
-  map(extract_patterns)
+l <- fs::dir_ls("tidal-drum-patterns/Sound/Tidal/Drum/") %>%
+  .[str_detect(., "All.hs", negate = T)] |>
+  map(extract_instr)
 
 names(l) <- str_remove(names(l), ".*/")
+df_name_repair <- tibble::tribble(
+  ~orig, ~strudel_name,
+  "cow",          "cb",
+  "ch",          "hh",
+  "rm",         "rim",
+  "ac",            NA,
+  "sn",          "sd",
+  "cy",            NA,
+  "cl",          "cp",
+  "sh",            NA,
+  "bd",          "bd",
+  "lt",          "lt",
+  "mt",          "mt",
+  "ht",          "ht",
+  "oh",          "oh",
+  "cp",          "cp",
+  "cb",          "cb"
+)
+
+lookuptable <- df_name_repair |>
+  mutate(strudel_name = coalesce(strudel_name, orig))
+
+rename_list_elements <- function(lst, lookuptable) {
+  names(lst) = lookuptable$strudel_name[match(names(lst), lookuptable$orig)]
+  lst
+}
 
 
-l_strudel <- l
+l_strudel <- l |>
+  map(~rename_list_elements(.x, lookuptable))
 names(l_strudel) <- paste0("strudel_beats/", str_replace(names(l), "hs$", "txt"))
+
+
+
+pattern_to_chr_vec <- function(pattern) {
+  pattern |>
+    str_replace_all("t", names(pattern)) %>%
+    paste0('"', ., '",')
+}
+
+
+
+
 write_strudel_file <- function(x, filename) {
   strudel_vec <- c(
     "// made by using the patterns from https://github.com/lvm/tidal-drum-patterns",
@@ -34,4 +74,6 @@ write_strudel_file <- function(x, filename) {
   write_lines(strudel_vec, filename)
 
 }
-l_strudel |> iwalk(~write_strudel_file(.x, .y))
+l_strudel |>
+  map(pattern_to_chr_vec) |>
+  iwalk(~write_strudel_file(.x, .y))
