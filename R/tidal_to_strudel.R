ecd8c03b5e29544819831459f4b4ac48d954640f
urswilke <urs.wilke@gmail.com>
2023-01-16 21:16:51 +0100

    add another method to export the patterns to a single file in the format as in https://github.com/tidalcycles/strudel/blob/main/website/src/repl/tunes.mjs

--- R/tidal_to_strudel.R
+++ R/tidal_to_strudel.R
@@ -18,7 +18,7 @@ l <- fs::dir_ls("tidal-drum-patterns/Sound/Tidal/Drum/") %>%
   .[str_detect(., "All.hs", negate = T)] |>
   map(extract_instr)
 
-names(l) <- str_remove(names(l), ".*/")
+names(l) <- str_remove(names(l), ".*/") |> str_remove("\\.hs$")
 df_name_repair <- tibble::tribble(
   ~orig, ~strudel_name,
   "cow",          "cb",
@@ -49,11 +49,10 @@ rename_list_elements <- function(lst, lookuptable) {
 
 l_strudel <- l |>
   map(~rename_list_elements(.x, lookuptable))
-names(l_strudel) <- paste0("strudel_beats/", str_replace(names(l), "hs$", "txt"))
 
 
 
-pattern_to_chr_vec <- function(pattern) {
+pattern_to_chr_vec_raw <- function(pattern) {
   pattern |>
     str_replace_all("t", names(pattern)) %>%
     paste0('"', ., '",')
@@ -62,8 +61,8 @@ pattern_to_chr_vec <- function(pattern) {
 
 
 
-write_strudel_file <- function(x, filename) {
-  strudel_vec <- c(
+format_strudel_vec <- function(x, filename) {
+  c(
     "// made by using the patterns from https://github.com/lvm/tidal-drum-patterns",
     "// (licensed under GPL-3.0 license)",
     "// using the R script from this repository: https://github.com/urswilke/read_beats",
@@ -71,9 +70,33 @@ write_strudel_file <- function(x, filename) {
     paste0("  ", x),
     ").s().slow(2)"
   )
-  write_lines(strudel_vec, filename)
-
 }
-l_strudel |>
-  map(pattern_to_chr_vec) |>
+
+
+write_strudel_file <- function(x, filename) {
+  write_lines(x, filename)
+}
+l_strudel1 <- l_strudel
+names(l_strudel1) <- paste0("strudel_beats/", names(l), ".txt")
+l_strudel1 |>
+  map(pattern_to_chr_vec_raw) |>
+  map(format_strudel_vec) |>
   iwalk(~write_strudel_file(.x, .y))
+
+
+write_strudel_string <- function(x, pattern_name) {
+  c(
+    paste0("export const ", pattern_name, " = `", x[1]),
+    x[-1],
+    "`;",
+    ""
+  )
+}
+
+all_patterns_file_content <- l_strudel |>
+  map(pattern_to_chr_vec_raw) |>
+  map(format_strudel_vec) %>%
+  # .[1:2] |>
+  imap(write_strudel_string) |> unlist() |> unname()
+
+write_lines(all_patterns_file_content, "all_strudel_beats.txt")
